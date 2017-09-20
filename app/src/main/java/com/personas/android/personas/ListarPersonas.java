0b0777ea9622b3af85cff36b31ade12533e51ea3
Carlos <carrieta6@cuc.edu.co>
2017-09-19 19:30:05 -0500

    Interfaz Lista
    Interfaz Lista

---
+++ app/src/main/java/com/personas/android/personas/ListarPersonas.java
@@ -0,0 +1,13 @@
+package com.personas.android.personas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListarPersonas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_personas);
+    }
+}
